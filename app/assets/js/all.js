ff23683a6253e717729597b5c65a8072d797cef1
olivier615 <52729795+olivier615@users.noreply.github.com>
2021-08-29 19:34:48 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,65 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+const swiper = new Swiper(".mySwiper", {
+  // loop:true,
+  slidesPerView:1,
+  spaceBetween: 30,
+  pagination: {
+    el:".swiper-pagination",
+    clickable: false,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
+
+const swiper2 = new Swiper(".mySwiper2", {
+  // loop:true,
+  slidesPerView:1,
+  spaceBetween: 30,
+  pagination: {
+    el:".swiper-pagination",
+    clickable: false,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1140: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  },
+
+});
